0afd1dc3c3bee76e33040f532dbd306b18bd4338
biktormo <biktormo@gmail.com>
2025-11-22 15:14:26 -0300

    firebase

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROYECTO.firebaseapp.com",
-  projectId: "TU_PROYECTO",
-  storageBucket: "TU_PROYECTO.appspot.com",
-  messagingSenderId: "...",
-  appId: "..."
+  apiKey: "AIzaSyCArk2WNb-sa919Q29Mo18BSagig2ewy8w",
+  authDomain: "inventario-d32b9.firebaseapp.com",
+  projectId: "inventario-d32b9",
+  storageBucket: "inventario-d32b9.firebasestorage.app",
+  messagingSenderId: "779687480646",
+  appId: "1:779687480646:web:78107fa0fec1d33413a947"
 };
 
 const app = initializeApp(firebaseConfig);
